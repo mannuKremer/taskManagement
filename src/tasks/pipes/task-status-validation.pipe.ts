9461406fd410d2da1f81e517d53352ad441987c2
mkremer2 <mannu.kremer@careboxhealth.com>
2019-11-02 18:54:42 +0200

    handlers and validations

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,24 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is an invalid status`);
+    }
+
+    return value;
+  }
+
+  isStatusValid(status): boolean {
+    const index = this.allowedStatuses.indexOf(status);
+    return index !== -1;
+  }
+}
